efb41c57f91ea169df90dad7021363b98bb3a059
illia.tikus <illiatikus@gmail.com>
2024-04-12 13:21:10 +0200

    small fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
 dependencies {
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
     // versions
-    val nav_version = "2.7.7"
+    val nav_version = "2.7.0"
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
@@ -75,5 +75,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("androidx.navigation:navigation-compose:$nav_version")
-    implementation ("com.google.android.exoplayer:exoplayer:2.16.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$nav_version")
+    implementation ("com.google.android.exoplayer:exoplayer:2.19.1")
 }
